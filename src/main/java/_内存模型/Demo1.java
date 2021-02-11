2c2fb2e2e7430fe284e350070aa7bd14e40fe455
lerry-desktop <chunang_li@163.com>
2021-02-11 19:26:56 +0800

    memory model learning...

---
+++ src/main/java/_内存模型/Demo1.java
@@ -0,0 +1,15 @@
+package _内存模型;
+
+/**
+ * @ClassName: Demo1
+ * @Author: lerry_li
+ * @CreateTime: 2021/02/11
+ * @Description
+ */
+public class Demo1 {
+    public static int add(int a, int b) {
+        int c = 0;
+        c = a + b;
+        return c;
+    }
+}
