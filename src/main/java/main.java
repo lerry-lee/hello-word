6561e3a6e161ea88a21378b1ab5b65c05fce4d30
lerry-desktop <chunang_li@163.com>
2021-09-13 15:35:10 +0800

    update

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,3 +1,4 @@
+import java.util.HashMap;
 import java.util.ListIterator;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.atomic.AtomicLong;
@@ -11,11 +12,10 @@ import java.util.concurrent.atomic.AtomicLong;
 public class main {
 
     public static void main(String[] args) {
-        int a = 128;
-        int b = 128;
-        System.out.println(a == b);
-        Integer c = 128;
-        Integer d = 128;
-        System.out.println(c == d);
+        HashMap<Integer,Integer> hashMap=new HashMap<>();
+        hashMap.put(null,1);
+        hashMap.put(1,2);
+        System.out.println(hashMap.get(null));
+        System.out.println(hashMap.containsKey(null));
     }
 }
